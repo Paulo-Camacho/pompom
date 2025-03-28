699ac8acd187cc0837b7d31e773439b8fe46ee36
Paulo-Camacho <anthropologyparliament@gmail.com>
2025-03-27 21:02:36 -0700

    The click from the user is working

--- Gui.java
+++ Gui.java
@@ -1,50 +1,46 @@
 package pompom;
 
-import java.util.Scanner;
-
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Gui
-{
-    public void menu()
-    {
+public class Gui {
+    private boolean debugPressed = false;  // This flag will be used to control flow
+
+    public void menu() {
         int windowX = 1920;
         int windowY = 1080;
 
-       JFrame frame = new JFrame( "pomodoro" );
-       frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-       frame.setSize( windowX, windowY );
+        JFrame frame = new JFrame("pomodoro");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(windowX, windowY);
 
-       JButton debug = new JButton( "debug" );
-       frame.setLayout(new GridLayout( 1, 1 ));
-       frame.getContentPane().add( debug );
+        JButton debug = new JButton("debug");
+        frame.setLayout(new GridLayout(1, 1));
+        frame.getContentPane().add(debug);
 
-       debug.addActionListener(null);
+        // Set up the ActionListener
+        debug.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                debugPressed = true;  // Set the flag to true when button is clicked
+                System.out.println("Debug button was clicked, starting display.");
+                display();  // Optionally, call display directly
+            }
+        });
 
         frame.setVisible(true);
-
-
-
     }
 
-
-    public void display( )
-    {
-        Scanner userIn = new Scanner( System.in );
-        System.out.println( "ENTER" );
-        while (!userIn.hasNextDouble())
-        {
-            System.out.println( "Invalid input. Please enter a number for seconds:" );
-            userIn.next();
+    public void display() {
+        if (debugPressed) {
+            System.out.println("Debugging mode activated, performing actions...");
+            // Implement the actions to take when debug mode is activated
+        } else {
+            System.out.println("Normal operation, continue as usual.");
         }
-        double length  = userIn.nextDouble( ) * 60;
-        Timer clock = new Timer( );
-        clock.start( length );
-        userIn.close( );
     }
+
 }
