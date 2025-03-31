2e22f810ed50a37ea20a8fb9960f6e2aefe2582f
Paulo-Camacho <anthropologyparliament@gmail.com>
2025-03-30 20:14:52 -0700

    A8 | So the Gui now works, time to make it rebust

--- Gui.java
+++ Gui.java
@@ -7,35 +7,42 @@ import javax.swing.*;
 import javax.swing.Timer;
 
 public class Gui implements ActionListener {
+
     private JFrame frame;
     private JButton debug;
-        private JButton rPom;
     private JLabel label;
     private JPanel panel;
     private Timer timer;
+    private int windowX = 1920;
+    private int windowY = 1080;
 
     private double time = 0.0;
 
     public Gui() {
         frame = new JFrame();
 
-        debug = new JButton( "Debug" );
-        rPom  = new JButton( "Run" );
-        debug.addActionListener(this);
-        rPom.addActionListener(this);
 
-        label = new JLabel("INIT");
+        frame.setLocationRelativeTo(null);
+        // buttons
+        debug = new JButton( "Debug" );
+        debug.setPreferredSize(new Dimension(250, 250));
+        debug.setFont(new Font("Arial", Font.PLAIN, 25));
+        label = new JLabel( "INIT" );
+        label.setFont(new Font("Arial", Font.PLAIN, 25));
+        label.setPreferredSize(new Dimension(250, 250));
+
+        debug.addActionListener(this);
 
         panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 50, 100));
-        panel.setLayout(new GridLayout(3, 1));
+        // Y, X, some sort of padding
+        panel.setBorder(BorderFactory.createEmptyBorder(500, 500, 500, 500));
+        panel.setLayout(new GridLayout( 1, 3 ));
         panel.add(debug);
-        panel.add(rPom);
         panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Check this out");
+        frame.setTitle( "Pomodoro Timer" );
         frame.pack();
         frame.setVisible(true);
 
